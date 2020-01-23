2d5d03034178fadaf154a9d9d0b55f8b938da2a9
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-01-23 16:26:35 +0400

    style : added @media query some style

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,8 +1,23 @@
 import React from "react";
 class Input extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            input : localStorage.getItem('input') ? localStorage.getItem('input') : '',
+        }
+    }
     componentDidMount() {
         this.nameInput.focus();
     }
+    
+    handleOnChange = (e) =>{
+       localStorage.setItem('input', e.target.value)
+       this.setState({
+           input : e.target.value
+       })
+       console.log('yes')
+    } 
+
     render() {
         return (
             <div>
@@ -11,6 +26,8 @@ class Input extends React.Component {
                     placeholder="What needs to be done?"
                     className="input"
                     id="todo-input"
+                    defaultValue={this.state.input}
+                    onChange={this.handleOnChange}
                     ref={input => {
                         this.nameInput = input;
                     }}
